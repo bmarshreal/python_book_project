fd409dbf8473550d53b58ff8ffa384ac3b119c67
bmarshreal <bmarshreal@yahoo.com>
2021-05-16 20:01:13 -0500

    First Build

--- app.py
+++ app.py
@@ -11,13 +11,51 @@ Enter:
 Your choice:
 """
 
+
 def menu():
     user_input = input(USER_CHOICE)
     while user_input!= 'q':
-        pass
+        if user_input == 'a':
+            prompt_add_book()
+        elif user_input == 'l':
+            list_books()
+        elif user_input == 'r':
+            prompt_read_book()
+        elif user_input == 'd':
+            prompt_delete_book()
+        else:
+            print("Unknown command. Please try again.")
+
+        user_input= input(USER_CHOICE)
 
 
-#def prompt_add_book() ,ask for book name and author
-#def list_books() ,show all the books in our list
-#def prompt_read_book() ,ask for book name and change it to "read" in our list
-#def prompt_delete_book() ,ask for book name and remove book from list
+def prompt_add_book():
+    name = input("Enter the new book name: ")
+    author = input("Enter the new book author: ")
+
+    database.add_book(name, author)
+
+
+def list_books():
+    books = database.get_all_books()
+    for book in books:
+        read = 'YES' if book['read'] else 'NO'
+        print(f"{book['name']} by {book['author']}, read: {read}")
+
+
+def prompt_read_book():
+    name = input("Enter the name of the book you've read.")
+
+    database.mark_book_as_read(name)
+
+
+def prompt_delete_book():
+    name = input("Please enter the name of the book you'd like to delete.")
+
+    database.delete_book(name)
+
+menu()
+# def prompt_add_book() ,ask for book name and author
+# def list_books() ,show all the books in our list
+# def prompt_read_book() ,ask for book name and change it to "read" in our list
+# def prompt_delete_book() ,ask for book name and remove book from list
--- utils/database.py
+++ utils/database.py
@@ -4,5 +4,28 @@ Concerned with storing and retrieving books from a list
 
 books = []
 
+
 def add_book(name, author):
-    books.append({'name':name, 'author':author, 'read':False })
+    books.append({'name':name, 'author':author, 'read':False})
+
+
+def get_all_books():
+    return books
+
+
+def mark_book_as_read(name):
+    for book in books:
+        if book['name'] == name:
+            book['read'] = True
+
+
+# def delete_book(name): #BAD PRACTICE to change a list while iterating over it!
+#     for book in books:
+#         if book['name'] == name:
+#             books.remove(book)
+
+def delete_book(name):
+    global books
+    # We need to state that the variable books is global
+    books = [book for book in books if book['name'] != name]
+    # Add each book to new list if ['name'] != name
